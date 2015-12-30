0651b468dbcde6e61b8d4f43f3f128e6ed347110
vasilie <vasilie.91@gmail.com>
2015-12-31 00:20:04 +0100

    merge

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,21 @@
 function scrollToTop(){
-  $("body").animate({scrollTop:0}, '500', 'swing');
+  var duration = 1500; //default
+  duration = $(window).scrollTop() /2.750+250;
+  console.log(duration);
+  $("body,html").animate({scrollTop:0}, duration , 'swing');
 }
 $(function(){
-	var back_to_top = $(".back-to-top"); 
+	var back_to_top = $(".back-to-top");
   $(window).scroll(function(){
    	if($(this).scrollTop()>=200){
 			back_to_top.addClass("visible");
    	} else {
-			back_to_top.removeClass("visible");   	
+			back_to_top.removeClass("visible");
    	}
-    	if ($(this).scrollTop()<=$("body").innerHeight()-848) {
-  			back_to_top.addClass("stick");
-  		} else {
-  			back_to_top.removeClass("stick");
-  		}
+  	if ($(this).scrollTop()<=$("body").innerHeight()-837) {
+			back_to_top.addClass("stick");
+		} else {
+			back_to_top.removeClass("stick");
+		}
   });
 });
